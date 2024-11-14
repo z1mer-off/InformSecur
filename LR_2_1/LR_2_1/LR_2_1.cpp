f3a0da80b459f07230737a73eec8bbf59c95a479
z1mer <101604219+z1mer-off@users.noreply.github.com>
2024-11-14 22:36:31 +0300

    Add files via upload

---
+++ LR_2_1/LR_2_1/LR_2_1.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//функция для проверки соответствия критериям Белла-Лападуллы
+bool checkBellLaPadula(int n, int m, const vector<vector<string>>& A, const vector<int>& LS, const vector<int>& LO) {
+    for (int i = 0; i < n; ++i) { //для каждого субъекта
+        for (int j = 0; j < m; ++j) { //для каждого объекта
+            //проверяем права доступа
+            if (A[i][j].find('r') != string::npos) { //если субъект имеет право на чтение
+                if (LS[i] < LO[j]) { //проверяем правило "Чтение сверху"
+                    cout << "Violation: subject s" << (i + 1) << " cannot read object o" << (j + 1) << endl;
+                    return false;
+                }
+            }
+            if (A[i][j].find('w') != string::npos) { //если субъект имеет право на запись
+                if (LS[i] > LO[j]) { //проверяем правило "Запись вниз"
+                    cout << "Violation: subject s" << (i + 1) << " cannot write in object o" << (j + 1) << endl;
+                    return false;
+                }
+            }
+        }
+    }
+    return true; //система соответствует критериям
+}
+
+int main() {
+    int n, m;
+
+    //ввод количества субъектов и объектов
+    cout << "Write a number of subjects (n): ";
+    cin >> n;
+    cout << "Write a number of objects (m): ";
+    cin >> m;
+
+    //ввод матрицы прав доступа
+    vector<vector<string>> A(n, vector<string>(m));
+    cout << "Write an access rights matrix (format: 'r', 'w' or nothing for lack of rights):" << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cout << "A[" << (i + 1) << "][" << (j + 1) << "]: ";
+            cin >> A[i][j];
+        }
+    }
+
+    //ввод уровней допуска субъектов
+    vector<int> LS(n);
+    cout << "Write an access levels of subjects (LS):" << endl;
+    for (int i = 0; i < n; ++i) {
+        cout << "LS[" << (i + 1) << "]: ";
+        cin >> LS[i];
+    }
+
+    //ввод уровней секретности объектов
+    vector<int> LO(m);
+    cout << "Write an secrecy levels of objects (LO):" << endl;
+    for (int j = 0; j < m; ++j) {
+        cout << "LO[" << (j + 1) << "]: ";
+        cin >> LO[j];
+    }
+
+    //проверка соответствия критериям Белла-Лападуллы
+    if (checkBellLaPadula(n, m, A, LS, LO)) {
+        cout << "CS meet the security criteria of Bella-Lapadull." << endl;
+    }
+    else {
+        cout << "CS does not meet the security criteria of Bella-Lapadull." << endl;
+    }
+
+    return 0;
+}
+/*пример при котором критерий не выполняется:
+    Input:
+3
+3
+
+r w n n r w w n r
+2 1 3
+1 2 3
+
+    Output:
+
+Violation: subject s2 cannot read object o2
+
+CS does not meet the security criteria of Bella-Lapadull.
+*/
---
+++ LR_2_4/LR_2_4/LR_2_4.cpp
@@ -0,0 +1,154 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    ifstream inputFile("access_matr.txt");
+    if (!inputFile) {
+        cerr << "File opening error!" << endl;
+        return 1;
+    }
+
+    int n, m;
+    inputFile >> n >> m;
+
+    //creating matrix of access rights
+    vector<vector<string>> accessMatrix(n, vector<string>(m));
+
+    //reading matrix of access rights
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            inputFile >> accessMatrix[i][j];
+        }
+    }
+
+    inputFile.close();
+
+    //1. Список объектов, доступ к которым не задан никому
+    vector<int> noAccessObjects;
+    for (int j = 0; j < m; ++j) {
+        bool hasAccess = false;
+        for (int i = 0; i < n; ++i) {
+            if (!accessMatrix[i][j].empty()) {
+                hasAccess = true;
+                break;
+            }
+        }
+        if (!hasAccess) {
+            noAccessObjects.push_back(j + 1); //Сохраняем номер объекта
+        }
+    }
+
+    //2. Список субъектов, не обращающихся ни к каким объектам
+    vector<int> noAccessSubjects;
+    for (int i = 0; i < n; ++i) {
+        bool hasAccess = false;
+        for (int j = 0; j < m; ++j) {
+            if (!accessMatrix[i][j].empty()) {
+                hasAccess = true;
+                break;
+            }
+        }
+        if (!hasAccess) {
+            noAccessSubjects.push_back(i + 1); //Сохраняем номер
+        }
+    }
+
+    //3. Список субъектов, имеющих полный доступ ко всем объектам
+    vector<int> fullAccessSubjects;
+    for (int i = 0; i < n; ++i) {
+        bool hasFullAccess = true;
+        for (int j = 0; j < m; ++j) {
+            if (accessMatrix[i][j] != "rw") {
+                hasFullAccess = false;
+                break;
+            }
+        }
+        if (hasFullAccess) {
+            fullAccessSubjects.push_back(i + 1); //Сохраняем номер
+        }
+    }
+
+    //4. Список C1, C2, …, Ct, где Ci – список субъектов, которые все имеют право записи в некоторый один объект oj
+    vector<vector<int>> writeConflictGroups;
+    for (int j = 0; j < m; ++j) {
+        vector<int> writers;
+        for (int i = 0; i < n; ++i) {
+            if (accessMatrix[i][j].find('w') != string::npos) {
+                writers.push_back(i + 1); //Сохраняем номер
+            }
+        }
+        if (!writers.empty()) {
+            writeConflictGroups.push_back(writers);
+        }
+    }
+
+    //5. Список субъектов, каждый из которых имеет полный доступ только к одному объекту
+    vector<int> singleAccessSubjects;
+    for (int i = 0; i < n; ++i) {
+        int accessCount = 0;
+        for (int j = 0; j < m; ++j) {
+            if (accessMatrix[i][j] == "rw") {
+                accessCount++;
+            }
+        }
+        if (accessCount == 1) {
+            singleAccessSubjects.push_back(i + 1); //Сохраняем номер
+        }
+    }
+
+    //Result outputing
+    cout << "Objects that no one has access to: ";
+    if (noAccessObjects.size() > 0) {
+        for (int obj : noAccessObjects) {
+            cout << obj << " ";
+        }
+    }
+    else {
+        cout << "None";
+    }
+    cout << endl;
+    cout << "Subjects that do not access any objects: ";
+    if (noAccessSubjects.size() > 0) {        
+        for (int subj : noAccessSubjects) {
+            cout << subj << " ";
+        }
+    }
+    else {
+        cout << "None";
+    }
+    cout << endl;
+
+    
+    cout << "Subjects that have full access to all objects: ";
+    if (fullAccessSubjects.size() > 0) {
+        for (int subj : fullAccessSubjects) {
+            cout << subj << " ";
+        }
+    }
+    else {
+        cout << "None";
+    }
+    cout << endl;
+
+    cout << "Subjects that all have the right to write to some one object: " << endl;
+    for (const auto& group : writeConflictGroups) {
+        cout << "Group of subj: ";
+        for (int subj : group) {
+            cout << subj << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Subjects that have full access to only one object: ";
+    for (int subj : singleAccessSubjects) {
+        cout << subj << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
